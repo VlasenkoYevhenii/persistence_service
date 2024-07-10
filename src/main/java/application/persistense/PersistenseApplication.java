c030f996600df00d3f794edec9ea5413a795012e
Yevhenii Vlasenko <yevhenii.vlasenko.work@gmail.com>
2024-07-10 13:12:17 +0300

    init

---
+++ src/main/java/application/persistense/PersistenseApplication.java
@@ -0,0 +1,13 @@
+package application.persistense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenseApplication.class, args);
+    }
+
+}
